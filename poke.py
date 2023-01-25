a69f8c3a44b00cad65db61a1dec961a42bdc31ec
gio7797 <gmontez650@gmail.com>
2023-01-24 17:37:39 -0800

    Create Trainer Class

--- poke.py
+++ poke.py
@@ -29,7 +29,9 @@ class Pokemon:
   def attack(self, opponent):
     if (self.race == "water" and opponent.race == "fire") or (self.race == "fire" and opponent.race == "grass") or (self.race == "grass" and opponent.race == "water"):
       print(self.name + " is super effective!")
+      #print attack message
       opponent.lose_health(opponent.level*2)
+      #update health
       opponent.cur_health -= opponent.level*2
       #Check if opponent health is less than 0
       if opponent.cur_health <= 0:
@@ -39,7 +41,9 @@ class Pokemon:
       
     elif (self.race == "water" and opponent.race == "grass") or (self.race == "fire" and opponent.race == "water") or (self.race == "grass" and opponent.race == "fire"):
       print(self.name + " is not effective!")
+      #print attack message
       opponent.lose_health(opponent.level/2)
+      #update health
       opponent.cur_health -= opponent.level/2
       #Check if opponent health is less than 0
       if opponent.cur_health <= 0:
@@ -48,10 +52,21 @@ class Pokemon:
         opponent.cur_health = 0
     else:
       print(self.name + " has used " + move_name)         # *** Add move_name instance variable *** #
+      #print attack message
       opponent.lose_health(opponent.level)
+      #update health
       opponent.cur_health -= opponent.level
       #Check if opponent health is less than 0
       if opponent.cur_health <= 0:
         print(opponent.name + " has feinted!")
         opponent.is_feinted = True
         opponent.cur_health = 0
+
+
+class Trainer:
+  def __init__(self, name):
+    pokemon = []
+    self.name = name
+    potions = 0
+    cur_act_poke = 0
+
