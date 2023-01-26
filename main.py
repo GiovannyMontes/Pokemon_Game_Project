1c18a9621be9161c5feb0881e893701a622a5559
gio7797 <gmontez650@gmail.com>
2023-01-25 20:36:08 -0800

    user input logic to choose starter pokemon

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from poke import Pokemon
 from poke import Trainer
 
+
 squirtle = Pokemon("squirtle", 8, "water")
 charmander = Pokemon("charmander", 7, "fire")
 bulbasaur = Pokemon("bulbasaur", 6, "grass")
 
-print(squirtle.level)
+gio = Trainer("gio")
+julie = Trainer("julie")
+
+
+####################### Make function ##########################
+print("Hello " + gio.name + " please choose your starter pokemon: ")
+starter_choice = input()
+
+if starter_choice == "squirtle":
+    gio.poke_lst.append(squirtle)
+elif starter_choice == "charmander":
+    gio.poke_lst.append(charmander)
+elif starter_choice == "bulbasaur":
+    gio.poke_lst.append(bulbasaur)
+else:
+    print("Invalid starter choice")
+################################################################
+
+
