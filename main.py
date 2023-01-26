ab4fed51e9da0a1fa17317585c0bc7d8bd4cadb4
gio7797 <gmontez650@gmail.com>
2023-01-25 20:32:28 -0800

    main

---
+++ main.py
@@ -0,0 +1,8 @@
+from poke import Pokemon
+from poke import Trainer
+
+squirtle = Pokemon("squirtle", 8, "water")
+charmander = Pokemon("charmander", 7, "fire")
+bulbasaur = Pokemon("bulbasaur", 6, "grass")
+
+print(squirtle.level)
